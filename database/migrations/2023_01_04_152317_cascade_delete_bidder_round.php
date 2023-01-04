ff6c317ff431f8a4c4857a8d2d274b837e4c5d64
Sebastian Walbrun <ws-1993@gmx.de>
2023-01-04 15:28:06 +0100

    fix: cascade delete offers when deleting a bidder round

---
+++ database/migrations/2023_01_04_152317_cascade_delete_bidder_round.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\BidderRound;
+use App\Models\Offer;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(Offer::TABLE, function(Blueprint $table) {
+            $table->dropForeign('offer_fkbidderround_foreign');
+        });
+        Schema::table(Offer::TABLE, function(Blueprint $table) {
+            $table->foreignId(BidderRound::class, Offer::COL_FK_BIDDER_ROUND)->onDelete('cascade');
+        });
+    }
+};
